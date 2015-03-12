9eab6ec1b6c1c09e559d8f24d0f02cecbcb8e518
Esteban Valery <evalery@dridco.com>
2015-03-12 14:44:06 -0300

    Removed unused imports

--- app/models/Settings.scala
+++ app/models/Settings.scala
@@ -1,17 +1,8 @@
 package models
 
-import com.typesafe.config.{Config, ConfigFactory}
-
 import scala.util.Properties
 
 class Settings() {
-//  constructor param: config: Config
-
-//  import scala.collection.JavaConverters._
-
-//  val mongoServers = config.getStringList("mongo.servers").asScala
-//  val mongoDbName  = config.getString("mongo.dbName")
-
   val mongoServers: List[String] = {
     val servers = Properties.envOrNone("MONGO_SERVERS")
     if (!servers.isDefined) throw new RuntimeException("Missing environment variable MONGO_SERVERS")
@@ -35,15 +26,10 @@ class Settings() {
     if (!password.isDefined) throw new RuntimeException("Missing environment variable MONGO_PASSWORD")
     password.get
   }
-
 }
 
 object Settings {
   def apply() = {
-//    ConfigFactory.load()
     new Settings()
   }
-//  def apply(conf: Config) = {
-//    new Settings(conf)
-//  }
 }
